dd9dcb13a18232645fbd7f3d6187ccd67adbc030
chikuBanda <chikubanda.cb@gmail.com>
2020-04-01 05:03:18 +0000

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur provident possimus necessitatibus ut, rerum praesentium. Sequi nesciunt blanditiis sit, officia nisi, delectus, vero tempore quod perspiciatis itaque praesentium esse doloribus!</p>
+@endsection
